90935ab61e4f03190162cbbdfa1ca7f4afe5ab39
Stephanie La Raja <stephanielaraja@gmail.com>
2021-05-18 08:10:50 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,10 +6,10 @@ describe 'new post' do
     expect(page.status_code).to eq(200)
   end
 
-#   it 'renders HTML in the /new template' do
-#     visit new_post_path
-#     expect(page).to have_content('Post Form')
-#   end 
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end 
 
 
   it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
